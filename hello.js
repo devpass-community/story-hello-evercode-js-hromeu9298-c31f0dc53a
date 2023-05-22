277ed5305e36bfa32c5ac0906fc824184ad4d596
Romeu maier <romeumaier@gmail.com>
2023-05-22 19:38:30 -0300

    Hello, World

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
-function hello(str) {
-    // Add your solution here!
-};
+function hello(name) {
+   return "Hello, " + name + "!!!"; 
+}
 
+var input = prompt("Digite seu nome:");
+var output = hello(input);
 module.exports = hello;
